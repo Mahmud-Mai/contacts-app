cb64c49e293e59e3cde544e3d333dda70221b3c0
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2022-09-20 17:04:19 +0100

    Added post handler And also included .env variables

--- server/index.js
+++ server/index.js
@@ -1,29 +1,43 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
-import Contact from './models/contact.js';
+import contact from './models/contact.js';
 
 const app = express();
+require('dotenv').config()
+const Contact = require('./models/contact.js')
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
  
-const CONNECTION_URL= 'mongodb+srv://contacts-admin:Defaultpassword@contactsappcluster.nkhy5bc.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000 ;
-
-mongoose.connect(CONNECTION_URL)
-.then( () => app.listen(PORT, () => console.log(`Connected to database successfully. Server is running on Port: ${PORT}`)) )
-.catch( (error) => console.log(error.message))
-
+console.log(process.env)
 
 app.get('/', (req, res) => {
-    const contact = new Contact({
-        name: 'new name',
-        number: '07012341234'
-    })
-    constact.save()
-     .then((result) => res.send(result))
+    Contact.find({})
+     .then((result) => {res.send(result)})
      .catch(err => console.log(err))
 })
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    const contactDetails = req.body;
+
+    if(contactDetails === undefined) {
+        return res.status(400).json({ error: 'content missing'})
+    }
+
+    const Contact = new Contact({
+        name: contactDetails.name,
+        number: contactDetails.number,
+    })
+
+    contact.save().then(savedNumber => {
+        response.json(savedNote)
+    })
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Connected to database successfully. Server is running on Port: ${PORT}`))
+
--- server/models/contact.js
+++ server/models/contact.js
@@ -1,5 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+
+const url = process.env.MONGODB_URL
+
+
+// mongoose.connect(url)
+// .then( () => app.listen(PORT, () => console.log(`Connected to database successfully. Server is running on Port: ${PORT}`)) )
+// .catch( (error) => console.log(error.message))
+
+
+console.log('connecting to ..', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
 
 const contactSchema = new Schema({
     name: {
@@ -12,6 +31,4 @@ const contactSchema = new Schema({
     }
 })
 
-const Contact = mongoose.model('Contact', contactSchema)
-export default Contact;
-module.exports = Contact;
+module.exports = mongoose.model('Contact', contactSchema)
